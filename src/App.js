eca6434f9b5b5040cc63b2b6b94d64ecba4961db
Jihad Noureddine <jihad.nd@gmail.com>
2020-10-12 17:44:02 +0300

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Karim"
+      name: "Tala and Jihad"
     };
   }
 
@@ -20,12 +20,92 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
-      </div>
-    );
+      <>
+            <div className="app">
+
+            <header>
+    <form action="">
+        <input class="search" type="text"/>
+        <input class="btn" type="button" value="Find Weather"/>
+    </form>
+</header>
+
+<section class="today-weather">
+            <img src="https://placekitten.com/g/100/100" alt="" srcset=""/>
+            <p class="weather-now">Overcast Clouds</p>
+
+            <div>
+                <h2>Temperature</h2>
+                <p>10º to 11ºC</p>
+                <p>
+                    <span class="today-weather-details-title">Humidity</span>
+                    <span class="today-weather-details-value">78%</span>
+
+                    <span class="today-weather-details-title">Pressure</span>
+                    <span class="today-weather-details-value">100848</span>
+                </p>
+            </div>
+        </section>
+
+        <section class="rest-of-today-weather">
+
+            
+            <div>
+                <p class="hour">3:00</p>
+                <img src="https://placekitten.com/g/100/100" alt="" srcset=""/>
+
+                <p class="temperature">8ºC</p>
+            </div>
+
+            
+            <div>
+                <p class="hour">3:00</p>
+                <img src="https://placekitten.com/g/100/100" alt="" srcset=""/>
+
+                <p class="temperature">8ºC</p>
+            </div>
+
+            
+            <div>
+                <p class="hour">3:00</p>
+                <img src="https://placekitten.com/g/100/100" alt="" srcset=""/>
+                <p class="temperature">8ºC</p>
+            </div>
+
+            
+            <div>
+                <p class="hour">3:00</p>
+                <img src="https://placekitten.com/g/100/100" alt="" srcset=""/>
+                <p class="temperature">8ºC</p>
+            </div>
+
+            
+            <div>
+                <p class="hour">3:00</p>
+            <img src="https://placekitten.com/g/100/100" alt="" srcset=""/>
+                <p class="temperature">8ºC</p>
+            </div>
+
+            
+            <div>
+                <p class="hour">3:00</p>
+            <img src="https://placekitten.com/g/100/100" alt="" srcset=""/>
+                <p class="temperature">8ºC</p>
+            </div>
+
+            
+            <div>
+                <p class="hour">3:00</p>
+            <img src="https://placekitten.com/g/100/100" alt="" srcset=""/>
+                <p class="temperature">8ºC</p>
+            </div>
+
+
+        </section>
+</div>
+      </>
+    )
+
   }
 }
 
